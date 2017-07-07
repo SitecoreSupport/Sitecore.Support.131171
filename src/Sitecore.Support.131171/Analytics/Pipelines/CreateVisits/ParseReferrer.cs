56e77db0c80ae7a835ca4b4385637f4e093bf9ce
DK\DTAT <dtat@sitecore.net>
2017-07-07 13:58:15 +0300

    Patch commit

---
+++ src/Sitecore.Support.131171/Analytics/Pipelines/CreateVisits/ParseReferrer.cs
@@ -0,0 +1,62 @@
+﻿namespace Sitecore.Support.Analytics.Pipelines.CreateVisits
+{
+  using Sitecore.Analytics.Pipelines.CreateVisits;
+  using Sitecore.Analytics.Pipelines.ParseReferrer;
+  using Sitecore.Analytics.Tracking;
+  using Sitecore.Diagnostics;
+  using System;
+  using System.Web;
+
+  public class ParseReferrer : CreateVisitProcessor
+  {
+    private void Parse(HttpRequestBase request, CurrentInteraction visit)
+    {
+      Uri urlReferrer;
+      Assert.ArgumentNotNull(request, "request");
+
+      try
+      {
+        urlReferrer = request.UrlReferrer;
+      }
+      catch
+      {
+        Log.Warn(string.Concat(new object[] { "Visit ", visit.InteractionId, ": referrer could not be parsed (", request.Headers["Referer"], ")" }), this);
+        urlReferrer = null;
+      }
+
+      if (urlReferrer == null)
+      {
+        visit.Keywords = string.Empty;
+        visit.ReferringSite = string.Empty;
+        visit.Referrer = string.Empty;
+      }
+      else if (urlReferrer.Host == HttpContext.Current.Request.Url.Host.ToString())
+      {
+        visit.Keywords = string.Empty;
+        visit.ReferringSite = string.Empty;
+        visit.Referrer = string.Empty;
+      }
+      else
+      {
+        visit.ReferringSite = urlReferrer.Host;
+        visit.Referrer = urlReferrer.ToString();
+        ParseReferrerArgs args = new ParseReferrerArgs
+        {
+          UrlReferrer = urlReferrer,
+          Interaction = visit
+        };
+        ParseReferrerPipeline.Run(args);
+        if (visit.Keywords == null)
+        {
+          visit.Keywords = string.Empty;
+        }
+      }
+    }
+
+    public override void Process(CreateVisitArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      this.Parse(args.Request, args.Interaction);
+    }
+  }
+}
---
+++ src/Sitecore.Support.131171/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.131171")]
+[assembly: AssemblyProduct("Sitecore.Support.131171")]
+[assembly: ComVisible(false)]
